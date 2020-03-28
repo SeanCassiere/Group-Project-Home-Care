8b878a681ccab580efece7079c35b73f19f69a24
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2020-03-28 15:25:21 +0530

    Added Facebook Chatbot

--- index.php
+++ index.php
@@ -15,6 +15,33 @@
 </head>
 <body>
 
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+window.fbAsyncInit = function() {
+  FB.init({
+    xfbml            : true,
+    version          : 'v6.0'
+  });
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+  attribution=install_email
+  page_id="111823250421038"
+  theme_color="#ff7e29"
+    logged_in_greeting="Welcome to HomeCare! How could I help you today?"
+    logged_out_greeting="Welcome to HomeCare! How could I help you today?">
+</div>
+
 <?php
 session_start();
 // IF Logged In show different navbar
