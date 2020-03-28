8653bba5c12b39697ff57efdb56e1ffce1682ffe
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2020-03-28 13:57:12 +0530

    Added: View Bookings UI for Maid

--- components/__bookings_maid_UI.php
+++ components/__bookings_maid_UI.php
@@ -6,7 +6,7 @@
       <caption>Current Bookings</caption>
       <thead class="thead-orange">
         <tr>
-          <th scope="col">#</th>
+          <th scope="col">Booking #</th>
           <th scope="col">Client</th>
           <th scope="col">Area</th>
           <th scope="col">Contact</th>
@@ -15,30 +15,40 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>John Doe</td>
-          <td>Wellawatta</td>
-          <td>+94 (0) 77-341-1909</td>
-          <td>2020-04-02</td>
-          <td>Rs. 800</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>John Doe</td>
-          <td>Dehiwela</td>
-          <td>+94 (0) 77-341-1909</td>
-          <td>2020-04-15</td>
-          <td>Rs. 1000</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>John Doe</td>
-          <td>Wellawatta</td>
-          <td>+94 (0) 77-341-1909</td>
-          <td>2020-04-26</td>
-          <td>Rs. 800</td>
-        </tr>
+        <?php
+        $maidId=$_SESSION["maid_Id"];
+        //echo $maidId;
+        require_once('__connect_db.php');
+
+        if (!$con) {
+          die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = "SELECT b.*, u.*, m.* FROM booking b";
+        $sql .= " INNER JOIN users u ON b.userId=u.userId";
+        $sql .= " INNER JOIN maid m ON b.maidId=m.maidID";
+        $sql .= " WHERE b.maidId='".$maidId."';";
+        $result = mysqli_query($con, $sql);
+        
+        if (mysqli_num_rows($result) > 0) {
+          // output data of each row
+          $rowNum = 1;
+          while($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+              echo "<th scope='row'>".$row['bookingRefNo']."</th>";
+              echo "<td>".$row['userName']."</td>";
+              echo "<td>".$row['maidAddress']."</td>";
+              echo "<td>+(94) ".$row['userPhone']."</td>";
+              echo "<td>".$row['bookingDate']."</td>";
+              echo "<td>Rs. ".(int)$row['maidServiceDailyRate']."</td>";
+            echo "</tr>";
+          }
+        } else {
+            echo "<tr>";
+            echo "<th scope='row' colspan='6'>No Booking have been made for you.</th>";
+            echo "</tr>";
+        }
+        ?>
       </tbody>
     </table>
   </div>
